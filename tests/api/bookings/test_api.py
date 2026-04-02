4b9a261f5dbd2f2436398834a079142321ad062a
nnikitenko <ninanineliya@gmail.com>
2026-04-02 13:09:33 +0300

    параметризованный тест

--- tests/api/bookings/test_api.py
+++ tests/api/bookings/test_api.py
@@ -1,18 +1,32 @@
-from starlette.status import HTTP_200_OK
+import pytest
+from starlette.status import HTTP_200_OK, HTTP_404_NOT_FOUND
 
 
-async def test_create_booking(auth_async_client, test_booking_data, register_user):
+@pytest.mark.parametrize(
+    argnames=("from_date", "to_date", "status_code"),
+    argvalues=[
+        ("2026-03-01", "2026-03-04", HTTP_200_OK),
+        ("2026-03-01", "2026-03-04", HTTP_200_OK),
+        ("2026-03-01", "2026-03-04", HTTP_200_OK),
+        ("2026-03-01", "2026-03-04", HTTP_200_OK),
+        ("2026-03-01", "2026-03-04", HTTP_200_OK),
+        ("2026-03-01", "2026-03-04", HTTP_404_NOT_FOUND),
+        ("2026-03-05", "2026-03-07", HTTP_200_OK),
+    ]
+)
+async def test_create_booking(auth_async_client, register_user, from_date, to_date, status_code):
     response = await auth_async_client.post(
         url="/bookings/",
         json={
-            "room_id": test_booking_data.room_id,
-            "from_date": str(test_booking_data.from_date),
-            "to_date": str(test_booking_data.to_date),
+            "room_id": 1,
+            "from_date": from_date,
+            "to_date": to_date,
         }
     )
-    assert response.status_code == HTTP_200_OK
-    response_data = response.json()
-    assert response_data["ok"] is True
-    assert response_data["data"]["room_id"]==test_booking_data.room_id
-    assert response_data["data"]["from_date"]==str(test_booking_data.from_date)
-    assert response_data["data"]["to_date"]==str(test_booking_data.to_date)
+    assert response.status_code == status_code
+    if status_code == HTTP_200_OK:
+        response_data = response.json()
+        assert response_data["ok"] is True
+        assert response_data["data"]["room_id"]==1
+        assert response_data["data"]["from_date"]==from_date
+        assert response_data["data"]["to_date"]==to_date
