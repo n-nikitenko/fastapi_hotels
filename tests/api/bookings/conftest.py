31d9dcb82473b48f4eb31b305790d0ada0fdac9c
nnikitenko <ninanineliya@gmail.com>
2026-04-02 13:40:18 +0300

    test_add_and_get_my_bookings

--- tests/api/bookings/conftest.py
+++ tests/api/bookings/conftest.py
@@ -2,6 +2,8 @@ from datetime import date
 
 import pytest
 
+from api.dependencies import get_db_manager
+from database import session_maker_null_pool
 from schemas import BookingAddEx
 
 
@@ -18,3 +20,9 @@ async def test_booking_data(db) -> BookingAddEx:
         user_id=user.id,
         price=6000,
     )
+
+@pytest.fixture(scope="module")
+async def remove_all_bookings():
+    async with get_db_manager(session_factory=session_maker_null_pool) as db:
+        await db.bookings.delete()
+        await db.commit()
--- tests/api/bookings/test_api.py
+++ tests/api/bookings/test_api.py
@@ -14,7 +14,7 @@ from starlette.status import HTTP_200_OK, HTTP_404_NOT_FOUND
         ("2026-03-05", "2026-03-07", HTTP_200_OK),
     ]
 )
-async def test_create_booking(auth_async_client, register_user, from_date, to_date, status_code):
+async def test_create_booking(auth_async_client, from_date, to_date, status_code):
     response = await auth_async_client.post(
         url="/bookings/",
         json={
@@ -30,3 +30,32 @@ async def test_create_booking(auth_async_client, register_user, from_date, to_da
         assert response_data["data"]["room_id"]==1
         assert response_data["data"]["from_date"]==from_date
         assert response_data["data"]["to_date"]==to_date
+
+@pytest.mark.parametrize(
+    argnames=("from_date", "to_date", "bookings_count"),
+    argvalues=[
+        ("2026-03-01", "2026-03-04", 1),
+        ("2026-03-01", "2026-03-04", 2),
+        ("2026-03-01", "2026-03-04", 3),
+        ("2026-03-01", "2026-03-04", 4),
+        ("2026-03-01", "2026-03-04", 5),
+        ("2026-03-01", "2026-03-04", 5),
+        ("2026-03-05", "2026-03-07", 6),
+    ]
+)
+async def test_add_and_get_my_bookings(remove_all_bookings, auth_async_client, from_date, to_date, bookings_count):
+    await auth_async_client.post(
+        url="/bookings/",
+        json={
+            "room_id": 1,
+            "from_date": from_date,
+            "to_date": to_date,
+        }
+    )
+
+    response = await auth_async_client.get(
+        url="/bookings/me"
+    )
+
+    assert response.status_code==HTTP_200_OK
+    assert len(response.json()) == bookings_count
